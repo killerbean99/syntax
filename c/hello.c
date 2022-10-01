ae47c9b5eadb1b6289eebe964a66ba7be1efea05
killerbean99 <yelaralaslac@gmail.com>
2022-10-02 01:12:46 +0600

    syntax folder

---
+++ c/hello.c
@@ -0,0 +1,22 @@
+/* this is comment */
+
+#include <stdio.h> // main library to input, output
+
+
+int main(void) {
+
+    printf("Hello, C !\n"); // also comment
+    return 0;
+
+    /* comment lines
+
+            1
+              2
+                3
+    
+    #include <stdio.h>
+
+    printf("Hello World\n");            
+    
+    */
+}
